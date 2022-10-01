17245aaa91b6e489349c79bd3e25956b7bd6609a
CM360 <cm360channel@gmail.com>
2022-09-30 21:08:45 -0400

    Don't use a removed asset

--- core/src/com/github/cm360/pixadv/graphics/picasso/Picasso.java
+++ core/src/com/github/cm360/pixadv/graphics/picasso/Picasso.java
@@ -60,7 +60,7 @@ public class Picasso {
 		batch.begin();
 		//
 		if (registry.isInitialized()) {
-			batch.draw(registry.getTexture(new Identifier("pixadv", "badlogic.jpg")), 0, 0, 200, 200);
+			batch.draw(registry.getTexture(new Identifier("pixadv", "textures/tiles/missing.png")), 0, 0, 200, 200);
 			renderWorld(universe);
 			renderGui();
 		} else {
