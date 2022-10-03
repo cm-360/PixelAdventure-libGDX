8580cd3df9f3065a48d3e13565860e4956f7050d
CM360 <cm360channel@gmail.com>
2022-10-03 17:26:34 -0400

    Reimplement button capabilities

--- core/src/com/github/cm360/pixadv/graphics/gui/components/generic/Button.java
+++ core/src/com/github/cm360/pixadv/graphics/gui/components/generic/Button.java
@@ -2,12 +2,42 @@ package com.github.cm360.pixadv.graphics.gui.components.generic;
 
 import com.github.cm360.pixadv.graphics.gui.components.BoundsMutator;
 import com.github.cm360.pixadv.graphics.gui.components.Component;
+import com.github.cm360.pixadv.registry.Identifier;
 
 public class Button extends Image {
 
-	public Button(Component parent, BoundsMutator boundsMutator) {
+	protected String text;
+	protected Identifier font;
+	protected int fontSize;
+	
+	public Button(Component parent, BoundsMutator boundsMutator, String text, Identifier font, int fontSize) {
 		super(parent, boundsMutator);
-		// TODO Auto-generated constructor stub
+		this.font = font;
+		this.fontSize = fontSize;
+	}
+	
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Identifier getFont() {
+		return font;
+	}
+
+	public void setFont(Identifier font) {
+		this.font = font;
+	}
+
+	public int getFontSize() {
+		return fontSize;
+	}
+
+	public void setFontSize(int fontSize) {
+		this.fontSize = fontSize;
 	}
 
 }
