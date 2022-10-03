83e04d1483aedec7efc5cc171f4cac798a46b45a
CM360 <cm360channel@gmail.com>
2022-10-03 17:26:18 -0400

    Reimplement image component texture support

--- core/src/com/github/cm360/pixadv/graphics/gui/components/generic/Image.java
+++ core/src/com/github/cm360/pixadv/graphics/gui/components/generic/Image.java
@@ -1,25 +1,32 @@
 package com.github.cm360.pixadv.graphics.gui.components.generic;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.github.cm360.pixadv.graphics.gui.components.BoundsMutator;
 import com.github.cm360.pixadv.graphics.gui.components.Component;
+import com.github.cm360.pixadv.registry.Identifier;
 import com.github.cm360.pixadv.registry.Registry;
 
 public class Image extends Component {
 
-	private Texture texture;
+	protected Identifier[] textures;
 	
 	public Image(Component parent, BoundsMutator boundsMutator) {
 		super(parent, boundsMutator);
 	}
 	
 	protected void paintSelf(SpriteBatch batch, Registry registry) {
-		batch.draw(texture,
-				(float) bounds.getX(),
-				(float) bounds.getY(),
-				(float) bounds.getWidth(),
-				(float) bounds.getHeight());
+		for (Identifier textureId : textures) {
+			batch.draw(ClientApplication.getRegistry().getTexture(textureId),
+					(float) bounds.getX(),
+					(float) bounds.getY(),
+					(float) bounds.getWidth(),
+					(float) bounds.getHeight());
+		}
+		
+	}
+	
+	public void setTextures(Identifier[] newTextures) {
+		textures = newTextures;
 	}
 
 }
