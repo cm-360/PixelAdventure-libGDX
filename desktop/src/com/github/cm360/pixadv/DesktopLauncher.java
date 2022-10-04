0bbca50fb49442f518eb2bcd61d3d66057ad5eeb
CM360 <cm360channel@gmail.com>
2022-10-04 19:29:31 -0400

    Set default window size/mode

--- desktop/src/com/github/cm360/pixadv/DesktopLauncher.java
+++ desktop/src/com/github/cm360/pixadv/DesktopLauncher.java
@@ -2,13 +2,13 @@ package com.github.cm360.pixadv;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.github.cm360.pixadv.ClientApplication;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		config.setWindowedMode(800, 500);
 		config.setTitle("PixelAdventure-libGDX");
 		new Lwjgl3Application(new ClientApplication(), config);
 	}
