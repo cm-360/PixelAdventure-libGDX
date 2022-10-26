05f5b52d3e2f587ad9a26b274f371b6cbc350ca6
cm360 <cm360@tron>
2022-10-26 13:30:34 -0400

    Create some required fields in universe and world classes

--- core/src/com/github/cm360/pixadv/environment/storage/Universe.java
+++ core/src/com/github/cm360/pixadv/environment/storage/Universe.java
@@ -4,7 +4,13 @@ import java.util.Map;
 
 public class Universe {
 
+	protected String name;
+	
 	protected Map<String, World> worlds;
+	
+	public String getName() {
+		return name;
+	}
 
 	public World getWorld(String name) {
 		return worlds.get(name);
--- core/src/com/github/cm360/pixadv/environment/storage/World.java
+++ core/src/com/github/cm360/pixadv/environment/storage/World.java
@@ -1,8 +1,26 @@
 package com.github.cm360.pixadv.environment.storage;
 
+import java.util.Map;
+import java.util.UUID;
+
+import com.github.cm360.pixadv.environment.types.Entity;
+import com.github.cm360.pixadv.registry.Identifier;
+
 public class World {
 
+	protected String name;
+	protected Identifier type;
+	
 	protected Chunk[][] chunks;
+	protected Map<UUID, Entity> entities;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Identifier getType() {
+		return type;
+	}
 	
 	public Chunk getChunk(int cx, int cy) {
 		return chunks[cx][cy];
@@ -10,6 +28,18 @@ public class World {
 	
 	public void setChunk(int cx, int cy, Chunk chunk) {
 		chunks[cx][cy] = chunk;
+	}
+	
+	public Entity getEntity(UUID uuid) {
+		return entities.get(uuid);
+	}
+	
+	public void addEntity(UUID uuid, Entity entity) {
+		entities.put(uuid, entity);
+	}
+	
+	public void removeEntity(UUID uuid) {
+		entities.remove(uuid);
 	}
 	
 }
