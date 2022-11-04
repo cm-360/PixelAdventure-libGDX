9849e37d221847e58d350fd1a77b564aa4b365d3
cm360 <cm360@tron>
2022-11-04 15:40:02 -0400

    Port old tile interface

--- core/src/com/github/cm360/pixadv/environment/types/Tile.java
+++ core/src/com/github/cm360/pixadv/environment/types/Tile.java
@@ -1,5 +1,19 @@
 package com.github.cm360.pixadv.environment.types;
 
-public class Tile {
+import java.util.List;
+
+import com.github.cm360.pixadv.registry.Identifier;
+
+public interface Tile {
+
+	public String getID();
+	
+	public void setData(String data);
+	
+	public String getData();
+	
+	public List<Identifier> getTextures();
+	
+	public double getFriction();
 
 }
