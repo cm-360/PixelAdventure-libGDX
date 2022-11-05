df9a10718504867304bfcd5b7ee2843344b47214
cm360 <cm360@tron>
2022-11-05 14:47:08 -0400

    Let chunks track lighting

--- core/src/com/github/cm360/pixadv/environment/storage/Chunk.java
+++ core/src/com/github/cm360/pixadv/environment/storage/Chunk.java
@@ -1,13 +1,16 @@
 package com.github.cm360.pixadv.environment.storage;
 
+import com.badlogic.gdx.graphics.Color;
 import com.github.cm360.pixadv.environment.types.Tile;
 
 public class Chunk {
 
 	protected Tile[][][] tiles;
+	protected Color[][] light;
 	
 	public Chunk(int size) {
 		tiles = new Tile[size][size][3];
+		light = new Color[size][size];
 	}
 	
 	public Tile getTile(int x, int y, int z) {
@@ -16,6 +19,14 @@ public class Chunk {
 	
 	public void setTile(Tile tile, int x, int y, int z) {
 		tiles[x][y][z] = tile;
+	}
+	
+	public Color getLight(int x, int y) {
+		return light[x][y];
+	}
+	
+	public void setLight(Color color, int x, int y) {
+		light[x][y] = color;
 	}
 
 }
